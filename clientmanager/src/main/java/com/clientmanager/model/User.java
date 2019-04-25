ee9cd0f0110a4b5a33cd3273dcafec5c04d59190
Ayla0 <michael.a.johnson0@gmail.com>
2019-04-25 11:30:01 -0400

    work on map<group,role> for user

--- clientmanager/src/main/java/com/clientmanager/model/User.java
+++ clientmanager/src/main/java/com/clientmanager/model/User.java
@@ -1,14 +1,20 @@
 package com.clientmanager.model;
 
+import java.util.HashMap;
 import java.util.Map;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.MapKey;
+import javax.persistence.MapKeyJoinColumn;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
@@ -39,23 +45,25 @@ public class User {
 	@NotNull
 	private char gender;
 
-	@ManyToMany()
-	@MapKey()
-	private Map<Group_, Role> job;
+	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@JoinTable(name = "_userToGroup_AndRole", joinColumns = { @JoinColumn(name = "user") }, inverseJoinColumns = {
+			@JoinColumn(name = "role") })
+	@MapKeyJoinColumn(name = "group")
+	private Map<Group_, Role> _userToGroup_AndRoleMappers = new HashMap<>();
 
 	public User() {
 		// TODO Auto-generated constructor stub
 	}
 
 	public User(int id, @NotNull String username, @NotNull String fname, String mname, @NotNull String lname,
-			@NotNull char gender, Map<Group_, Role> job) {
+			@NotNull char gender, Map<Group_, Role> _userToGroup_AndRoleMappers) {
 		this.id = id;
 		this.username = username;
 		this.fname = fname;
 		this.mname = mname;
 		this.lname = lname;
 		this.gender = gender;
-		this.job = job;
+		this._userToGroup_AndRoleMappers = _userToGroup_AndRoleMappers;
 	}
 
 	public int getId() {
@@ -106,18 +114,18 @@ public class User {
 		this.gender = gender;
 	}
 
-	public Map<Group_, Role> getJob() {
-		return job;
+	public Map<Group_, Role> get_userToGroup_AndRoleMappers() {
+		return _userToGroup_AndRoleMappers;
 	}
 
-	public void setJob(Map<Group_, Role> job) {
-		this.job = job;
+	public void set_userToGroup_AndRoleMappers(Map<Group_, Role> _userToGroup_AndRoleMappers) {
+		this._userToGroup_AndRoleMappers = _userToGroup_AndRoleMappers;
 	}
 
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", username=" + username + ", fname=" + fname + ", mname=" + mname + ", lname="
-				+ lname + ", gender=" + gender + ", job=" + job + "]";
+				+ lname + ", gender=" + gender + ", _userToGroup_AndRoleMappers=" + _userToGroup_AndRoleMappers + "]";
 	}
 
 }
