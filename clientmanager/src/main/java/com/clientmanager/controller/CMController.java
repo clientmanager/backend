c05cb72a99a0b6257f95720187ea66f78aea43cd
Nick-Manglaviti <nickmango64@gmail.com>
2019-05-01 09:44:36 -0400

    Added CrossOrigin to communicate between front and backend

--- clientmanager/src/main/java/com/clientmanager/controller/CMController.java
+++ clientmanager/src/main/java/com/clientmanager/controller/CMController.java
@@ -3,6 +3,7 @@ package com.clientmanager.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import com.clientmanager.service.RoleService;
 import com.clientmanager.service.TeamService;
 import com.clientmanager.service.UserService;
 
+@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*")
 @RestController
 public class CMController {
 
